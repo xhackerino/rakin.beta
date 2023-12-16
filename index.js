79bc2a32970daba307db28c602985a1931369902
xhackerino <superilyushagrief@gmail.com>
2023-12-17 02:10:03 +0300

    Fixed input validation

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-// Function to convert Hex to RGB
+// Function to convert Hex to RGB with error handling
 function convertHexToRGB() {
     let hex = document.getElementById('hexInput').value;
+    if (!isValidHex(hex)) {
+        document.getElementById('rgbResult').innerText = "Invalid Hex Code";
+        return;
+    }
     let rgb = hexToRgb(hex);
-    let resultText = rgb ? `RGB: (${rgb.r}, ${rgb.g}, ${rgb.b})` : "Invalid Hex Code";
+    let resultText = `RGB: (${rgb.r}, ${rgb.g}, ${rgb.b})`;
     document.getElementById('rgbResult').innerText = resultText;
 }
 
-// Function to convert RGB to Hex
+// Function to convert RGB to Hex with error handling
 function convertRGBToHex() {
-    let r = document.getElementById('redInput').value;
-    let g = document.getElementById('greenInput').value;
-    let b = document.getElementById('blueInput').value;
-    let hex = rgbToHex(parseInt(r), parseInt(g), parseInt(b));
+    let r = parseInt(document.getElementById('redInput').value);
+    let g = parseInt(document.getElementById('greenInput').value);
+    let b = parseInt(document.getElementById('blueInput').value);
+    if (isNaN(r) || isNaN(g) || isNaN(b) || !isValidRgb(r, g, b)) {
+        document.getElementById('hexResult').innerText = "Invalid RGB Values";
+        return;
+    }
+    let hex = rgbToHex(r, g, b);
     document.getElementById('hexResult').innerText = `Hex: ${hex}`;
+}
+
+// Helper function to validate Hex code
+function isValidHex(hex) {
+    return /^#[0-9A-F]{6}$/i.test(hex);
+}
+
+// Helper function to validate RGB values
+function isValidRgb(r, g, b) {
+    return r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255;
 }
 
 // Helper function to convert Hex to RGB
